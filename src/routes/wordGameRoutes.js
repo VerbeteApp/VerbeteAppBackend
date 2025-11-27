90c3b8903e97f27bebb3136c860ae11724ec29ee
Antonio Costa <antonioclaudiocostajr@gmail.com>
2025-11-27 14:49:09 -0300

    Changes done by copilot and wordlist endpoint created

--- src/routes/wordGameRoutes.js
+++ src/routes/wordGameRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const wordGameController = require('../controller/wordGameController');
 
 router.get('/', wordGameController.getDailyWord)
+router.get('/wordList', wordGameController.getAllWordsFromList)
 
 module.exports = router;
 
